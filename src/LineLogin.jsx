4be47e00901d0669a191d584322542a5ff4c49d0
phuwadech <aofphuwadech@gmail.com>
2024-06-26 14:01:51 +0700

    add line login

---
+++ src/LineLogin.jsx
@@ -0,0 +1,27 @@
+import {useEffect} from "react";
+import {liff} from "@line/liff";
+
+function LineLogin() {
+
+    useEffect(() => {
+        liff.init({
+            liffId : '2004757924-K09NzeWv'
+        })
+            .then(()=>{
+                if (!liff.isLoggedIn()){
+                    liff.login({});
+                }
+            })
+            .catch((err)=>{
+                console.log(err.code,err.message);
+            })
+    }, []);
+
+
+
+    return (
+        <div>Line Login</div>
+    );
+}
+
+export default LineLogin;
--- src/route.jsx
+++ src/route.jsx
@@ -1,8 +1,10 @@
 import {createBrowserRouter} from "react-router-dom";
 import App from "./App.jsx";
 import Test from "./Test.jsx";
+import LineLogin from "./LineLogin.jsx";
 
 export const routes = createBrowserRouter([
     {path: '/',element: <App/>},
     {path: '/test',element: <Test/>},
+    {path: 'line-login',element: <LineLogin/>}
 ])
