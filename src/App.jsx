b5a5877e5f493d6aa518876aaa700146a87b8383
phuwadech <aofphuwadech@gmail.com>
2024-06-26 14:11:12 +0700

    add button login with line

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import {Link} from "react-router-dom";
 
 function App() {
   const [count, setCount] = useState(0)
@@ -17,17 +18,22 @@ function App() {
         </a>
       </div>
       <h1>Vite + React + Ozoneaik</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+        <div className="card">
+            <button onClick={() => setCount((count) => count + 1)}>
+                count is {count}
+            </button>
+            <Link to={'line-login'}>
+                <button style={{background: "green"}}>
+                    Login With Line
+                </button>
+            </Link>
+            <p>
+                Edit <code>src/App.jsx</code> and save to test HMR
+            </p>
+        </div>
+        <p className="read-the-docs">
+            Click on the Vite and React logos to learn more
         </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
