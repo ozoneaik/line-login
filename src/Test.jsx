bf7017aac010dde0383ef9d4af4beffa64fb563a
phuwadech <aofphuwadech@gmail.com>
2024-06-26 13:42:34 +0700

    add Test Page

---
+++ src/Test.jsx
@@ -0,0 +1,7 @@
+function Test() {
+    return (
+        <div>Test Page</div>
+    );
+}
+
+export default Test;
--- src/route.jsx
+++ src/route.jsx
@@ -1,6 +1,8 @@
 import {createBrowserRouter} from "react-router-dom";
 import App from "./App.jsx";
+import Test from "./Test.jsx";
 
 export const routes = createBrowserRouter([
-    {path: '/',element: <App/>}
+    {path: '/',element: <App/>},
+    {path: '/',element: <Test/>},
 ])
